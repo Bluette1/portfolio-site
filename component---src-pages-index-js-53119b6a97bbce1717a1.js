8a9e594eb499c29ba421f23abe29590433bb4dc2
Bluette1 <marylene.sawyer@gmail.com>
2024-09-25 05:29:54 +0200

    Updates

---
+++ component---src-pages-index-js-53119b6a97bbce1717a1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{6558:function(e,t,a){a.r(t);var l=a(7294),r=a(1883);t.default=()=>l.createElement("div",{className:"home"},l.createElement("header",null,l.createElement("h1",null,"Marylene Sawyer"),l.createElement("h2",null,"Web Developer"),l.createElement("p",null,"Creating beautiful, user-friendly websites."),l.createElement(r.Link,{to:"/projects",className:"cta"},"View My Work")))}}]);
+//# sourceMappingURL=component---src-pages-index-js-53119b6a97bbce1717a1.js.map
